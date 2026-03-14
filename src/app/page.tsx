2eb30c80922dfd00e569c37b21f6a185ce0458b5
Tripleiks <heino.romzykowski@live.de>
2026-03-15 00:38:52 +0100

    feat: Featured Section auf maximal 3 Posts begrenzt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default async function Home() {
   // Alle MDX-Posts zur Build-Zeit laden
   const posts = getAllPosts();
   
-  // Posts mit sticky=true aus dem Frontmatter filtern
-  const stickyPosts = posts.filter(post => post.sticky);
+  // Posts mit sticky=true aus dem Frontmatter filtern (maximal 3)
+  const stickyPosts = posts.filter(post => post.sticky).slice(0, 3);
   // Alle Posts für "all-posts" (inkl. sticky posts)
   const allPosts = posts;
 

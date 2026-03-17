613a7cc76436ac080f56bd2509e5f50791a170b0
Tripleiks <heino.romzykowski@live.de>
2026-03-17 19:40:01 +0100

    fix: wrap handleServerFunctions as server action for RootLayout

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -1,9 +1,9 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 /* DO NOT MODIFY IT UNLESS YOU KNOW WHAT YOU ARE DOING. */
 import { RootLayout } from '@payloadcms/next/layouts'
-import { handleServerFunctions } from '@payloadcms/next/layouts'
 import config from '@payload-config'
 import { importMap } from './admin/importMap'
+import { serverFunction } from './actions'
 import React from 'react'
 
 import '@payloadcms/next/css'
@@ -11,8 +11,6 @@ import '@payloadcms/next/css'
 type Args = {
   children: React.ReactNode
 }
-
-const serverFunction = handleServerFunctions
 
 export default function Layout({ children }: Args) {
   return (
